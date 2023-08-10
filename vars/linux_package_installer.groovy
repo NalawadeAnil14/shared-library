5be82152f704ffd9ff586cb3e5738951da2dbabd
Ubuntu <ubuntu@ip-172-31-95-215.ec2.internal>
2023-08-10 04:16:55 +0000

    change

--- vars/linux_package_installer.groovy
+++ vars/linux_package_installer.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+import org.apache.commons.lang.StringUtils
+
 def install(String package) {
   def unzipInstalled = sh(script: 'which $package', returnStatus: true)
                     def packageManager = ""
